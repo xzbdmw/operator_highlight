e0cb648451f5345d3cfbcb77bb9a491810cb4798
GitHub Action <action@github.com>
2023-03-24 05:30:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/xzbdmw/operatorhighlight/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/xzbdmw/operatorhighlight/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.xzbdmw.operatorhighlight.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.xzbdmw.operatorhighlight.MyBundle
+import com.github.xzbdmw.operatorhighlight.services.MyProjectService
 import javax.swing.JButton
 
 
